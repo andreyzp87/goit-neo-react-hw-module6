05ffea0f0a56429e6c69d79d98fe5c7f2d6be270
Andy <andreyzp87@gmail.com>
2024-09-08 23:56:15 +0300

    implement task

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import { useSelector } from 'react-redux';
+import { selectContacts } from './redux/contactsSlice';
+
+function App() {
+  const contacts = useSelector(selectContacts);
+  return (
+    <div className="app">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      {contacts.length > 0 && <SearchBox />}
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
